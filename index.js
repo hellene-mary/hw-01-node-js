f632d8441abe1d392080cfe6a130fde8d8d568d1
Grek Maria <grekmariaa@gmail.com>
2022-12-22 14:23:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ program.parse(process.argv)
 
 const argv = program.opts()
 
-// TODO: рефакторить
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
